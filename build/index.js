849eb64bd5f07a13ad2a1295df8edcc9ab5149d6
marcos <marcosaureliofarias2023@gmail.com>
2021-12-29 11:22:25 -0300

    feature/elb/

---
+++ build/index.js
@@ -0,0 +1,57 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongodb_1 = __importDefault(require("./database/mongodb"));
+const Form_1 = __importDefault(require("./models/Form"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+try {
+    const databaseUriUrl = "mongodb+srv://Thrower:thrower@cluster0.xzqic.mongodb.net/HuntingIA-Dev?retryWrites=true&w=majority";
+    (0, mongodb_1.default)(databaseUriUrl);
+}
+catch (e) {
+    console.error(e.message);
+    process.exit(0);
+}
+const PORT = 8000;
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send("Bem-Vindo PrimeControl");
+}));
+app.post("/users-form/create", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    try {
+        const formCreated = yield Form_1.default.create(Object.assign({}, body));
+        res.status(201).json(formCreated);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error.message,
+        });
+    }
+}));
+app.get("/users-form", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const forms = yield Form_1.default.find();
+        res.status(201).json(forms);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error.message,
+        });
+    }
+}));
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
